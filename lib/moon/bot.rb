0a70746df780c9267b148221a9f9b378ca7449e9
Aliou Diallo <code@aliou.me>
2014-08-14 18:59:31 +0200

    Setup twitter.

--- lib/moon/bot.rb
+++ lib/moon/bot.rb
@@ -5,19 +5,33 @@ require 'twitter'
 module Moon
   class Bot
     FILE = 'moon.txt'
+
     def run
       data = File.read FILE
       lines = data.split "----\n"
       lines.shift
 
       idx = index
-      puts lines[idx]
+      tweet!(lines[idx])
       save!(idx + 1)
     end
 
+    def reset!
+      Moon.redis.del('index')
+    end
+
     private
+
     def index
       Moon.redis.get('index').to_i or 0
+    end
+
+    def tweet!(text)
+      if ENV['RACK_ENV'] == 'production'
+        Moon.twitter.update(text)
+      else
+        puts text
+      end
     end
 
     def save!(index)
